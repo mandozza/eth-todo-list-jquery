a33ee1a755fee2740b88cdc7369f86057030761f
Jeremy Josey <webjeremy@gmail.com>
2020-06-09 09:46:46 -0300

    Completed Project

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -12,6 +12,17 @@ contract TodoList {
     }
     //Sets up basically an array to hold all the data.
     mapping(uint => Task) public tasks;
+    // Creates an event to notify something has happened.
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+    //Creates an event to notify task boolean has changed.
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
 
     // Runs on deploy
     constructor() public {
@@ -22,6 +33,18 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        // Kicks off an event.
+        emit TaskCreated(taskCount, _content, false);
+    }
 
+    function toggleCompleted(uint _id) public {
+        // create the variable in memory (_ means its a local variable)
+        Task memory _task = tasks[_id];
+        // flip  the bool
+        _task.completed = !_task.completed;
+        // put it back into the mapping.
+        tasks[_id] = _task;
+        // Emit event that it was completed.
+        emit TaskCompleted(_id, _task.completed);
     }
 }
